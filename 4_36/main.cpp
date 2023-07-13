457bb712caebf19a773be053f771c1eb504cf192
VtripleG <eagleofdead1337@gmail.com>
2023-07-13 12:20:55 +0300

    Выполнен номер 4.36

---
+++ 4_36/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    double t;
+
+    std::cout << "Enter time in minuets: ";
+    std::cin >> t;
+
+    if ( ( std::fmod(t, 5) >= 0 ) && ( std::fmod(t, 5) <= 3 ) )
+        std::cout << "GREEN" << std::endl;
+    else
+        std::cout << "RED" << std::endl;
+
+    return 0;
+}

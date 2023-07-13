3fd9fddb728c6927e17b5633e69346f16dcd0723
VtripleG <eagleofdead1337@gmail.com>
2023-07-13 16:38:25 +0300

    Сделано задание 4.141

---
+++ 4_141/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    int countFlat, startNumber;
+    int sumFlat;
+
+    std::cout << "Enter count and start number of flat" << std::endl;
+    std::cin >> countFlat >> startNumber;
+
+    for ( int count = startNumber; count <= startNumber + countFlat; count++)
+        sumFlat+=count;
+
+    if ( sumFlat % 2 == 0 )
+        std::cout << "Even" << std::endl;
+    else
+        std::cout << "Uneven" << std::endl;
+
+    return 0;
+}

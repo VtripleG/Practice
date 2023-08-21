b13734068b435d87867d0d0b22e04b5a8aee41f9
VtripleG <eagleofdead1337@gmail.com>
2023-08-21 10:46:23 +0300

    Complite 13.54

--- 13_54/main.cpp
+++ 13_54/main.cpp
@@ -1,53 +1,167 @@
 #include <iostream>
 #include <vector>
 
+void line()
+{
+  for ( int count = 0; count < 100; ++count )
+    std::cout << "-";
+
+  std::cout << std::endl;
+}
+
+class Score
+{
+public:
+  int scoredBalls;
+  int missedBalls;
+
+  Score ()
+  {
+    scoredBalls = 0;
+    missedBalls = 0;
+  }
+};
+
+class Team
+{
+public:
+  int pointsScore = 0;
+  std::vector <Score> scoreList;
+
+  Team ( const int teamsNumber )
+  {
+    for ( int count = 0; count < teamsNumber; ++count )
+      scoreList.push_back( Score() );
+  }
+};
+
+int DrawNumber ( std::vector <Team>& teamsList )
+{
+  int drawCount = 0;
+
+  for ( int teamCount = 0; teamCount < teamsList.size(); ++teamCount )
+    for ( int gameCount = teamCount; gameCount < teamsList.size(); ++gameCount )
+    {
+      if ( gameCount != teamCount )
+        if ( teamsList[teamCount].scoreList[gameCount].scoredBalls == teamsList[teamCount].scoreList[gameCount].missedBalls )
+          drawCount++;
+    }
+
+  return drawCount;
+
+}
+
+int DifferenceNumber ( std::vector <Team>& teamsList )
+{
+  int differenceCount = 0;
+
+  for ( int teamCount = 0; teamCount < teamsList.size(); ++teamCount )
+    for ( int gameCount = teamCount; gameCount < teamsList.size(); ++gameCount )
+    {
+      if ( gameCount != teamCount )
+        if ( teamsList[teamCount].scoreList[gameCount].scoredBalls - teamsList[teamCount].scoreList[gameCount].missedBalls >= 3
+             || teamsList[teamCount].scoreList[gameCount].scoredBalls - teamsList[teamCount].scoreList[gameCount].missedBalls <=
+             -3 )
+          differenceCount++;
+    }
+
+  return differenceCount;
+}
+
+void PointsScore ( std::vector <Team>& teamsList )
+{
+  for ( int teamCount = 0; teamCount < teamsList.size(); ++teamCount )
+    for ( int gameCount = 0; gameCount < teamsList.size(); ++gameCount )
+    {
+      if ( gameCount != teamCount )
+      {
+        if ( teamsList[teamCount].scoreList[gameCount].scoredBalls > teamsList[teamCount].scoreList[gameCount].missedBalls )
+          teamsList[teamCount].pointsScore += 3;
+        else if ( teamsList[teamCount].scoreList[gameCount].scoredBalls =
+                    teamsList[teamCount].scoreList[gameCount].missedBalls )
+          teamsList[teamCount].pointsScore += 1;
+      }
+    }
+}
+
+void WhoIsChampion ( std::vector <Team>& teamsList )
+{
+  int championNumber = 0;
+  int championScore = teamsList[0].pointsScore;
+
+  for ( int teamCount = 1; teamCount < teamsList.size(); ++teamCount )
+    if ( teamsList[teamCount].pointsScore > championScore )
+    {
+      championScore = teamsList[teamCount].pointsScore;
+      championNumber = teamCount;
+    }
+
+  std::cout << "Champion is " << championNumber + 1 << " team, scored " << championScore << " points" << std::endl;
+
+}
+
 int main()
 {
-  std::vector <std::vector <std::vector <int>>> teamList;
-  const int teamNum = 4;
-  const int gamesNum = 4;
+  const int teamsNumber = 4;
+  const int gamesNumber = 4;
 
-  for ( int count = 0; count < teamNum; ++count )
-  {
-    std::vector <std::vector <int>> buff;
-    std::vector <int> buffScore;
-    teamList.push_back( buff );
-    teamList[count].push_back( buffScore );
-    teamList[count].push_back( buffScore );
-  }
+  std::vector <Team> teamsList;
 
-  for ( int count = 0; count < teamNum; ++count )
-  {
-    for ( int gameCount = 0; gameCount < gamesNum; ++gameCount )
+  for ( int count = 0; count < teamsNumber; ++count )
+    teamsList.push_back( Team( teamsNumber ) );
+
+  for ( int teamCount = 0; teamCount < teamsNumber; ++teamCount )
+    for ( int gameCount = teamCount; gameCount < gamesNumber; ++gameCount )
     {
-      if ( gameCount != count )
+      if ( gameCount != teamCount )
       {
-        std::cout << "Enter scored ball in " << gameCount + 1 << " game of " << count + 1 << " team: ";
-        int scoredBalls;
-        std::cin >> scoredBalls;
-        std::cout << "Enter missed ball in " << gameCount + 1 << "game of " << count + 1 << " team: ";
-        int missedBalls;
-        std::cin >> missedBalls;
-        teamList[count][0].push_back( scoredBalls );
-        teamList[count][1].push_back( missedBalls );
-      }
-      else
-      {
-        teamList[count][0].push_back( 0 );
-        teamList[count][1].push_back( 0 );
-      }
+        std::cout << "Team " << teamCount + 1 << ", game " << gameCount + 1 << std::endl;
+        int buffScored = 0;
+        std::cout << "Enter scored balls: ";
+        std::cin >> buffScored;
 
+        int buffMissed = 0;
+        std::cout << "Enter missed balls: ";
+        std::cin >> buffMissed;
+
+        teamsList[teamCount].scoreList[gameCount].scoredBalls = buffScored;
+        teamsList[teamCount].scoreList[gameCount].missedBalls = buffMissed;
+
+        teamsList[gameCount].scoreList[teamCount].scoredBalls = buffMissed;
+        teamsList[gameCount].scoreList[teamCount].missedBalls = buffScored;
+      }
+    }
+
+  line();
+
+  for ( int teamCount = 0; teamCount < teamsNumber; ++teamCount )
+  {
+    for ( int gameCount = 0; gameCount < gamesNumber; ++gameCount )
+    {
+      std::cout << "Team " << teamCount + 1 << ", game " << gameCount + 1 << std::endl;
+      std::cout << "Scoread balls: " << teamsList[teamCount].scoreList[gameCount].scoredBalls << std::endl;
+      std::cout << "Scoread balls: " << teamsList[teamCount].scoreList[gameCount].missedBalls << std::endl;
     }
   }
 
-  for ( int count = 0; count < teamNum; ++count )
-  {
-    std::cout << "team " << count + 1 << std::endl;
+  line();
 
-    for ( int gameCount = 0; gameCount < gamesNum; ++gameCount )
-      std::cout << "game " << gameCount + 1 << ": " << teamList[count][0][gameCount] << " " << teamList[count][1][gameCount]
-                << std::endl;
-  }
+  std::cout << "All draw number: " << DrawNumber( teamsList ) << std::endl;
+
+  line();
+
+  std::cout << "Difference => 3: " << DifferenceNumber ( teamsList ) << std::endl;
+
+  line();
+
+  PointsScore( teamsList );
+
+  for ( int teamCount = 0; teamCount < teamsNumber; ++teamCount )
+    std::cout << "Points score team " << teamCount + 1 << ": " << teamsList[teamCount].pointsScore << std::endl;
+
+  line();
+
+  WhoIsChampion( teamsList );
 
   return 0;
 }
